68d38e217dd361f90f319ac044d53914db42e841
val <valeria@gmail.com>
2024-09-03 06:55:28 -0500

    feat: se agg validaciones al propietario

--- Taller/models/Propietario.cs
+++ Taller/models/Propietario.cs
@@ -10,11 +10,19 @@ namespace Taller.models
     {
         [Key]
         public int Id { get; set;}
+        [MinLength(5, ErrorMessage = "the name field must be at least {1} characters")]
+        [MaxLength(125, ErrorMessage = "")]
+
         public required string Nombre { get; set; }
         public required string Apellido { get; set; }
         public required string NumeroDeIdentificacion { get; set; }
         public required string Direccion { get; set; }
         public required string Telefono { get; set;}
+
+
+        [EmailAddress(ErrorMessage = "")]
+        [MinLength(5,ErrorMessage ="")]
+
         public required string Correo  { get; set; }
     }
 }
